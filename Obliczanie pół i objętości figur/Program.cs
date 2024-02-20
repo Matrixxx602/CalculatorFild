f30bbb55399888a46e1133aa4d34ede43e0c0604
Mateusz Orzel <matrixxx602@o2.pl>
2024-02-20 11:31:44 +0100

    Po edycji goto -> while, lecz z błedem.

--- Obliczanie pół i objętości figur/Program.cs
+++ Obliczanie pół i objętości figur/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Policy;
 
 namespace Obliczanie_pól_i_objętości_figur
 {
@@ -7,143 +8,124 @@ namespace Obliczanie_pól_i_objętości_figur
         static void Main(string[] args)
         {
             // Zmienne
-            string wybor;
-            float r, a, b, h, duzeh, obj;
-            float polef = 0;
+            string choice;
+            float area, bigheight, vol;
 
             // Wybór figury geometrycznej
             Console.WriteLine("Witaj w programie obliczjącym pola i objętości figur.");
             Console.WriteLine("Wybierz jedną z figur: okrag, kwadrat, prostokat, trojkat.");
 
             // Obliczanie pola figury
+            FigureField pole = new FigureField();
             skip:
-            wybor = Console.ReadLine();
-            Console.WriteLine("Wybrałeś {0}", wybor);
-            PoleFigur pole = new PoleFigur();
+            choice = Console.ReadLine();
 
-             
-           switch (wybor)
+           switch (choice)
            {
                     case "okrag":
-                        pole.Okrag(out r, out polef);
+                        area = pole.CalculateCircle();
                         break;
 
                     case "kwadrat":
-                        pole.Kwadrat(out a, out polef);
+                       area = pole.CalculateSquare();
                         break;
 
                     case "prostokat":
-                        pole.Prostokat(out a, out b, out polef);
+                       area = pole.CalculateRectangle(out bool isIt);
+
+                        if (isIt)
+                        {
+                        Console.WriteLine("Ten prostokat jest kwadratem.");
+                        }
                         break;
 
                     case "trojkat":
-                        pole.Trojkat(out a, out h, out polef);
+                        area = pole.CalculateTriangle();
                         break;
+
                     default:
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Podałeś złe wyrazenie, popraw!");
+                        Console.WriteLine("Podales zle wyrazenie, popraw!");
                         Console.ResetColor();
                         Console.WriteLine("Masz do wyboru: okrag, kwadrat, prostokat, trojkat.");
                         goto skip;
 
            }
-            
-           
+            Console.WriteLine("Pole figury wynosi: {0}", area);
+
             // Obliczanie objętości
             Console.Write("Teraz policzymy objętość bryły. Podaj jego wysokość: ");
             skip2:
-            bool prawidlowawys = float.TryParse(Console.ReadLine(), out duzeh);
-            if (duzeh == 0)
+            bool value = float.TryParse(Console.ReadLine(), out bigheight);
+            if (bigheight <= 0)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Zła wartość! Podaj wlasciwa.");
+                Console.ResetColor();
                 goto skip2;          
             }
 
-            obj = polef * duzeh;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Objetość bryły o podstawie {0} to: {1}",wybor, obj);
-            Console.ResetColor();
+            vol = area * bigheight;
+            Console.WriteLine("Objetość bryły o podstawie {0} to: {1}",choice, vol);
         }
     }
-    public class PoleFigur
+    public class FigureField
     {
-        internal float Okrag(out float promien, out float polef)
+        internal float CalculateCircle()
         {
-            float pi = 3.1415f;
+            const float pi = 3.1415f;
 
-            step1:
-            Console.Write("Podaj promien okręgu: ");
-            bool wynik = float.TryParse(Console.ReadLine(),out promien);
+            var r = UserInputHelper.GetValue("Podaj promien okręgu: ");
 
-            if (promien == 0)
-            {
-                goto step1;
-            }
-
-            polef = pi * (promien * promien);
-            Console.WriteLine("Pole okregu wynosi: {0}", polef);
-            return polef;
+            return pi * (r * r);
         }
 
-        internal float Kwadrat(out float bok, out float polef)
+        internal float CalculateSquare()
         {
-            step1:
-            Console.Write("Podaj dlugosc boku: ");
-            bool wynik = float.TryParse(Console.ReadLine(), out bok);
+            float side1 = UserInputHelper.GetValue("Podaj bok kwadratu");
 
-            if (bok == 0)
-            {
-                goto step1;
-            }
-
-            polef = bok * bok;
-            Console.WriteLine("Pole kwadratu wynosi: {0}", polef);
-            return polef;
+            return side1 * side1;
         }
 
-        internal float Prostokat(out float bok1, out float bok2, out float polef)
+        internal float CalculateRectangle(out bool isSquare)
         {
-            step1:
-            Console.Write("Podaj dlugosc pierwszego boku: ");
-            bool wynik1 = float.TryParse(Console.ReadLine(), out bok1);
-            if (bok1 == 0)
+            isSquare = false;
+
+            float side1 = UserInputHelper.GetValue("Podaj pierwszy bok prostokata: ");
+            var side2 = UserInputHelper.GetValue("Podaj drugi bok prostokata: ");
+           
+
+            if (side1 == side2)
             {
-                goto step1;
+                isSquare = true;
             }
 
-            step2:
-            Console.Write("Podaj dlugosc drugiego boku: ");
-            bool wynik2 = float.TryParse(Console.ReadLine(), out bok2);
-            if (bok2 == 0)
-            {
-                goto step2;
-            }
-
-            polef = bok1 * bok2;
-            Console.WriteLine("Pole prostokatu wynosi: {0}", polef);
-            return polef;
+            return side1 * side2;
         }
 
-        internal float Trojkat(out float podstawa, out float wysokosc, out float polef)
+        internal float CalculateTriangle()
         {
-            step1:
-            Console.Write("Podaj dlugosc podstawy: ");
-            bool wynik1 = float.TryParse(Console.ReadLine(), out podstawa);
-            if (podstawa == 0)
-            {
-                goto step1;
-            }
+            float side1 = UserInputHelper.GetValue("Podaj dlugosc podstawy trojkata: ");
+            var height = UserInputHelper.GetValue("Podaj wysokosc trojkata: ");
+          
+            return side1 * height * 0.5f;
+        }
+    }
+    public static class UserInputHelper
+    {
 
-            step2:
-            Console.Write("Podaj wysokosc trojkata: ");
-            bool wynik2 = float.TryParse(Console.ReadLine(), out wysokosc);
-            if (wysokosc == 0)
-            {
-                goto step2;
-            }
+        public static float GetValue(string question)
+        {
+            Console.WriteLine(question);
 
-            polef = podstawa * wysokosc * 0.5f;
-            Console.WriteLine("Pole trojkata wynosi: {0}", polef);
-            return polef;
+            while (!float.TryParse(Console.ReadLine(), out float result) || result <= 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Zla wartosc! Podaj wlasciwa.");
+                Console.ResetColor();
+                Console.WriteLine(question);
+            }
+            return result;
         }
     }
 }
